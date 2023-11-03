d760f40fd0fb452670e6b648e0924ed3d576f94e
Bekidemissie <bekidemissie16@gmail.com>
2023-11-03 17:38:22 -0400

    adding PhotoDetailsModal

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoDetailsModal from './PhotoDetailsModal';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = () => {
+  // State to manage if the modal is visible
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  // Function to open the modal
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <div className="home-route">
-      { <TopNavigationBar/>}
+      <TopNavigationBar />
+      {/* Button to open modal */}
+      <button onClick={openModal}>Show Photo Details</button>
+      
+      {/* Conditionally render the PhotoDetailsModal */}
+      {isModalVisible && <PhotoDetailsModal closeModal={closeModal} />}
     </div>
   );
 };
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-
-import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ photo, closeModal }) => {
+  if (!photo) return null;
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+      <button className="photo-details-modal__close-button" onClick={closeModal}>
+        <img src={closeSymbol} alt="Close" />
       </button>
+      <div className="photo-details-content">
+        <img src={photo.urls.full} alt={photo.user.name} />
+        {/* Other details here */}
+      </div>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
